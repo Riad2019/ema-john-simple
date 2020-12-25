655937c65b29d2801d5c610ebea01237a7b18d28
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2020-12-25 15:04:04 +0600

    process

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,7 +4,7 @@ import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const first10 = fakeData.slice[0,10];
+    const first10 = fakeData.slice(0,10);
     const  [products ,setProduct ]= useState(first10);
     return (
         <div className="shop-container">
